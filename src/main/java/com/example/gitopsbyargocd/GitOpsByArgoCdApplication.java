1ae024628542ba7bc1eb8da72a2eb74e10e7f417
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2021-12-21 14:44:08 +0100

    Build a new version by log at startup time of spring boot

--- src/main/java/com/example/gitopsbyargocd/GitOpsByArgoCdApplication.java
+++ src/main/java/com/example/gitopsbyargocd/GitOpsByArgoCdApplication.java
@@ -1,13 +1,18 @@
 package com.example.gitopsbyargocd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitOpsByArgoCdApplication {
+public class GitOpsByArgoCdApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GitOpsByArgoCdApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application Version 1.1 is Started.");
+    }
 }
